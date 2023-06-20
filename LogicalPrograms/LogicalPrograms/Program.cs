b3f28b7722170e4ad2eba2fd1f56fc6cdd2b7a62
“Niketanjenekar” <niketanjenekar31@gmail.com>
2023-06-20 14:12:49 +0530

    [Niketan]Add:Welcome to Logiacl Programs

---
+++ LogicalPrograms/LogicalPrograms/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("*************Welcome to Logical Programs**************");
+            
+            Console.ReadLine();
+        }
+    }
+}
